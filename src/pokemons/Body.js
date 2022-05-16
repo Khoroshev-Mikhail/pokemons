60e16cc431a04b264c9810a080720c150903283f
Mikha38 <oyks1379@yandex.ru>
2022-05-16 15:14:29 +0300

    home

---
+++ src/pokemons/Body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './body.css'
+
+class Body extends React.Component{
+    render(){
+        return <div className='main'>
+            Hello world!!!
+            </div>
+    }
+}
+export default Body
