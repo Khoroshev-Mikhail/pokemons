b03b5596beb4f035ef54fa76d5f512fecb4e471b
Mikha38 <oyks1379@yandex.ru>
2022-05-17 21:23:48 +0300

    home

--- src/pokemons/Body.js
+++ src/pokemons/Body.js
@@ -4,8 +4,73 @@ import './body.css'
 class Body extends React.Component{
     render(){
         return <div className='main'>
-            Hello world!!!
+            <header className='header'> 
+                <h2 className='header__topH2'>Поймано покемонов</h2>
+                <h1 className='header__botH1'>5/16</h1>
+            </header>
+            <div className='pokemonsgrid'>
+
+                <div className='pokemon'>
+                    <div className='pokemon__name'>name</div>
+                    <div className='pokemon__img'>
+                        <img />
+                    </div>
+                    <div className='pokemon__button'>
+                        <button>Поймать</button>
+                    </div>
+                </div>
+
+                <div className='pokemon'>
+                    <div className='pokemon__name'>name</div>
+                    <div className='pokemon__img'>
+                        <img />
+                    </div>
+                    <div className='pokemon__button'>
+                        <button>Поймать</button>
+                    </div>
+                </div>
+
+                <div className='pokemon'>
+                    <div className='pokemon__name'>name</div>
+                    <div className='pokemon__img'>
+                        <img />
+                    </div>
+                    <div className='pokemon__button'>
+                        <button>Поймать</button>
+                    </div>
+                </div>
+
+                <div className='pokemon'>
+                    <div className='pokemon__name'>name</div>
+                    <div className='pokemon__img'>
+                        <img />
+                    </div>
+                    <div className='pokemon__button'>
+                        <button>Поймать</button>
+                    </div>
+                </div>
+
+                <div className='pokemon'>
+                    <div className='pokemon__name'>name</div>
+                    <div className='pokemon__img'>
+                        <img />
+                    </div>
+                    <div className='pokemon__button'>
+                        <button>Поймать</button>
+                    </div>
+                </div>
+
+                <div className='pokemon'>
+                    <div className='pokemon__name'>name</div>
+                    <div className='pokemon__img'>
+                        <img />
+                    </div>
+                    <div className='pokemon__button'>
+                        <button>Поймать</button>
+                    </div>
+                </div>
             </div>
+        </div>
     }
 }
 export default Body
