2411c6fadb0ee1370111c833d9b358a70f7b8206
Mikha38 <oyks1379@yandex.ru>
2022-05-22 19:40:43 +0300

    home

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,47 +1,48 @@
 import React from "react";
 
-
 class Form extends React.Component{
     constructor(){
         super()
         this.state = {
-            bgcolor: 'blue',
-            inputValue: '',
-            inputBorderColor: 'black'
+            bgcolor: `white`,
+            values: {
+                inputValue: '',
+                secondInputValue: '',
+                qwer: '',
+            },
         }
     }
     handlerForm(e){
         e.preventDefault()
-        const regexp = /^[a-z0-9]{6}$/i
-        if(!regexp.test(this.state.inputValue)){
-            alert('Ввод должен быть в формате #1F11FF')
-        } else {
-            this.setState(({bgcolor}) => {
-                return {bgcolor: this.state.inputValue}
-            })
-        }
+        this.setState(({values}) => ({ bgcolor: `linear-gradient(${Object.values(values).join(', ')})`}))
     }
-    handlerInput(e){
-        const regexp = /^[a-z0-9]{6}$/i
-        if(regexp.test(e.target.value)){
-            this.setState(({inputBorderColor})=>{
-                return {inputBorderColor: 'green'}
-            })
-        }else{
-            this.setState(({inputBorderColor}) => {
-                return {inputBorderColor : 'red'}
-            })
-        }
-        this.setState(({inputValue}) => {
-            return {inputValue : e.target.value}
-        })
+    //ДЗ сверстать меню
+    //Скопировать данные из json
+    //Отрисовать менюшку из json через state.
+
+    handlerValues(e){
+        const {name, value} = e.target
+
+        this.setState(({values}) => ({
+            values: {...values, [name]: value}
+        }))
+    }
+
+    reset(){
+        
+        this.setState(({values}) => ({
+            values: Object.fromEntries(Object.keys(values).map((key) => [key, ""]))
+        }))
     }
 
     render(){
-        return <div style={{width: '1000px', height: '200px', background: `#${this.state.bgcolor}`, border: '1px solid black', margin: '0px auto'}}>
+        return <div style={{width: '1000px', height: '200px', background: this.state.bgcolor, border: '1px solid black', margin: '0px auto'}}>
             <form onSubmit={this.handlerForm.bind(this)}>
-                <input type='text' onChange={this.handlerInput.bind(this)} style={{borderColor: this.state.inputBorderColor, borderWidth: '5px', borderStyle: 'solid'}}></input>
-                <input type='submit' value="Задать фон" ></input>
+                {Object.entries(this.state.values).map(([name, value]) => {
+                    return <input type='text' name={name} onChange={this.handlerValues.bind(this)} value={value}/>
+                })}
+                <button type="button" onClick={this.reset.bind(this)}>Reset</button>
+                <button type="submit">Go!</button>
             </form>            
         </div>
     }
