fbf930a02932f488128f3a9bbecd48890581ec4f
Mikha38 <oyks1379@yandex.ru>
2022-05-28 21:45:15 +0300

    home

--- src/Menu/MenuList/MenuList.js
+++ src/Menu/MenuList/MenuList.js
@@ -1,48 +1,55 @@
-import React from "react";
+import React, { useCallback, useContext } from "react";
 
 class MenuList extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            childrenVisible: false
+            visibleLists : {},
+            isLoaded: {},
         }
     }
     checkUnderEl(el, data){
         let result = []
-        
-        for(let underEl in data.entities.pages){
-            if(data.entities.pages[underEl].parentId === el.id){
-                result.push(data.entities.pages[underEl])
+        if('pages' in el){
+            for(let child of el.pages){
+                result.push(data.entities.pages[child])
             }
         }
-        /*
-        if(data.entities.pages[el].hasOwnProperty('pages')){
-            for(let child in data.entities.pages[el].pages){
-                result.push(data.entities.pages[el].pages[child])
-            }
-        }
-        */
-        /*for(let child in data.entities.pages[el].pages){
-            result.push(data.entities.pages[el].pages[child])
-        }*/
         
         return result
     }
     toggleVisible(e){
         const id = e.target.id
-        this.setState((state)=>{
-            return {[id] : state[id] === undefined ? true : !state[id]}
-        })
+        if(!this.state.visibleLists[id]){
+            this.setState(({isLoaded}) => ({
+                isLoaded: { ...isLoaded, [id] : true}
+            }))
+        }
         console.log(this.state)
+        function ara(){
+            this.setState(({visibleLists, isLoaded}) => ({
+                visibleLists: { ...visibleLists, [id] : visibleLists[id] === undefined ? true : !visibleLists[id]},
+                isLoaded : { ...isLoaded, [id] : false} //Выполняется каждый раз, даже если isLoaded[id] === false
+            }))
+        }
+        setTimeout(ara.bind(this), 500)
+
     }
     render(){
         return <ul className="menu__list">
             {this.props.list.map(el => {
                 const underElements = this.checkUnderEl(el, this.props.data)
                 if(underElements.length > 0){
-                    return <li className={this.state[el.id] ? 'menu__list__liWithArrowUp' : 'menu__list__liWithArrowDown'}> 
-                        <span id={el.id} onClick={this.toggleVisible.bind(this)}>{el.title}</span>
-                        {this.state[el.id] ? <MenuList list={underElements} data={this.props.data}/> : ''}
+                    return <li className={this.state.visibleLists[el.id]  ? 'menu__list__liWithArrowUp' : 'menu__list__liWithArrowDown'}> 
+                        <span id={el.id} onClick={this.toggleVisible.bind(this)}>
+                            {el.title}
+                        </span>
+                        {this.state.isLoaded[el.id] ?
+                            <div>
+                                <h3>LOADING!PLEASE WAIT!</h3>
+                            </div> : ''
+                        }
+                        {this.state.visibleLists[el.id] ? <MenuList list={underElements} data={this.props.data}/> : ''}
                     </li>
                 } else{
                     return <li>{el.title}</li>
