262253b94b28f307cea4d526cfda7f2ff25dc674
Mikha38 <oyks1379@yandex.ru>
2022-07-07 19:50:09 +0300

    home

---
+++ src/pokemons/Redux/middlewares2.js
@@ -0,0 +1,69 @@
+const initialState = {
+    value1: 1,
+    value2: 2,
+}
+const reducer1 = (state = initialState.value1, action) => {
+    console.log('im here')
+    if(action.type === "VALUE1"){
+        return action.value
+    }
+    return state
+}
+const reducer2 = (state = initialState.value2, action) => {
+    if(action.type === "VALUE2"){
+        return action.value
+    }
+    return state
+}
+const combineReducers = (obj) => {
+    return (state, action) => {
+        let newState = {}
+        for(let prop in obj){
+            newState[prop] = obj[prop](state[prop], action)
+        }
+        return newState
+    }
+}
+const reducers = combineReducers({
+    value1: reducer1,
+    value2: reducer2
+})
+
+const createStore = (reducer, enchancer) => {
+    if(enchancer !== undefined){
+        return enchancer(createStore)(reducer)
+    }
+    let state = reducers({}, {})
+    let store = {
+        getState: () => state,
+        dispatch: (action) => state = reducer(state, action)
+    }
+    return store
+}
+const layer1 = (store) => (next) => (action) => {
+    const result = next(action)
+    console.log('Layer1 is here!')
+    return result
+}
+const layer2 = (store) => (next) => (action) => {
+    const result = next(action)
+    console.log('Layer2 is here!')
+    return result
+}
+const applyMiddleware = (...middlewares) => (createStore) => (reducers) => {
+    const store = createStore(reducers)
+    const { dispatch } = store
+
+    const enchancedDispatch = (action) => {
+        return middlewares.reduceRight((acc, next) => next(store)(acc), dispatch)(action)
+    }
+
+    return {
+        ...store,
+        dispatch: enchancedDispatch
+    }
+}
+
+const store = createStore(reducers, applyMiddleware(layer1, layer2))
+store.dispatch({type: "VALUE1", value: 100})
+console.log(store.getState())
